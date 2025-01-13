b04d5490d5c0a4deb27b12b4baa5f88fa0718a39
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2025-01-13 17:07:02 +0530

    .

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,65 +1,67 @@
-const CategoryModel = require("../Model/Catrgory_Model");
-const UserModel = require("../Model/UserModel");
-const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require('../Model/UserModel');
+const CategoryModel = require('../Model/CategoryModel');
 
 
 const Form = async (req, res) => {
   try {
-    const { username, email, password, role, secretkey } = req.body
+    const { username, email, password, role, secretkey } = req.body;
 
     const oldUser = await UserModel.findOne({ email });
     if (oldUser) {
       return res.status(400).send({ data: "User already exists" });
     }
 
-   
-    if (role === "admin") {
-      if (process.env.ADMIN_SECRET_KEY !== secretkey) {
-        return res.status(403).send({ data: "You are not authorized" });
-      }
+    if (role === "admin" && process.env.ADMIN_SECRET_KEY !== secretkey) {
+      return res.status(403).send({ data: "You are not authorized" });
     }
 
-    
-    const newUser = await UserModel.create({ username, email, password, role });
-    return res.status(201).send({ data: "User created successfully", data: newUser });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const newUser = await UserModel.create({ username, email, password: hashedPassword, role });
+
+    return res.status(201).send({ data: "User created successfully", user: newUser });
   } catch (error) {
     console.error(error.message);
     return res.status(500).send({ data: "An error occurred", error: error.message });
   }
 };
 
-
-const login = async (req, res) => {
+ const login = async (req, res) => {
   try {
-    const { email, password } = req.body
-   
-     const userdata = await UserModel.findOne({ email });
+    const { email, password } = req.body;
+
+    const userdata = await UserModel.findOne({ email });
 
     if (!userdata) {
-      return res.send({ data : "User Not register" })
+      return res.status(404).send({ data: "User not registered" });
     }
- 
-    const token = jwt.sign({ userId: userdata._id, userRole: userdata.role }, "SID")
-    console.log(token);
 
-    return res.send({ data: "user login successfully" , token : token })
-  }
-  catch (error) {
+    const isPasswordValid = await bcrypt.compare(password, userdata.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).send({ data: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: userdata._id, userRole: userdata.role }, "SID");
+
+    return res.status(200).send({ data: "User login successfully", token });
+  } catch (error) {
     console.log(error);
-
+    return res.status(500).send({ data: "An error occurred", error: error.message });
   }
-}
-const login_Get = async(req,res) =>{
-    const data = await UserModel.find()
-    // console.log(data)
-    res.send({data})
-    
-}
-const getall_data = async (req, res) => {
-  const data = await CategoryModel.find().populate("categoryes")
-  // console.log(data)
-  res.send({ data })
+};
 
-}
-module.exports = { Form, getall_data, login , login_Get }
+
+const login_Get = async (req, res) => {
+  const data = await UserModel.find();
+  res.send({ data });
+};
+
+const getall_data = async (req, res) => {
+  const data = await CategoryModel.find().populate("categoryes");
+  res.send({ data });
+};
+
+module.exports = { Form, login, login_Get, getall_data };
