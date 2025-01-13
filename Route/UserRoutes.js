88301b8bee25a6a65957adfd6d8d4b008078b578
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2025-01-13 17:38:59 +0530

    .

--- Route/UserRoutes.js
+++ Route/UserRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const { Form, getall_data, login, login_Get } = require('../Controller/UserController')
+const { Form, login, login_Get } = require('../Controller/UserController')
 const UserRoute = express.Router()
 UserRoute.post("/form",Form)
 UserRoute.post("/login",login)
 UserRoute.get("/login",login_Get)
-UserRoute.get("/getall",getall_data)
+
 module.exports=UserRoute
