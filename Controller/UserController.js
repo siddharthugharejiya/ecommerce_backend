6d8097f91dab2804cc7d04042159d0d84d5226cd
bhaviksirvi20 <bhaviksirvi964@gmail.com>
2025-03-31 13:33:15 +0530

    .

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,13 +1,14 @@
-
+require('dotenv').config(); // Load environment variables
 const jwt = require('jsonwebtoken');
 const UserModel = require('../Model/UserModel');
-
-
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 const Form = async (req, res) => {
   try {
+    console.log(process.env.ADMIN_SECRET_KEY); // Log to check the value
+
     const { username, email, password, role, secretkey } = req.body;
+    console.log(req.body);
 
     const oldUser = await UserModel.findOne({ email });
     if (oldUser) {
@@ -19,7 +20,6 @@ const Form = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = await UserModel.create({ username, email, password: hashedPassword, role });
 
     return res.status(201).send({ data: "User created successfully", user: newUser });
@@ -29,24 +29,21 @@ const Form = async (req, res) => {
   }
 };
 
- const login = async (req, res) => {
+const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const userdata = await UserModel.findOne({ email });
-
     if (!userdata) {
       return res.status(404).send({ data: "User not registered" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, userdata.password);
-
     if (!isPasswordValid) {
       return res.status(401).send({ data: "Invalid credentials" });
     }
 
     const token = jwt.sign({ userId: userdata._id, userRole: userdata.role }, "SID");
-
     return res.status(200).send({ data: "User login successfully", token });
   } catch (error) {
     console.log(error);
@@ -54,11 +51,9 @@ const Form = async (req, res) => {
   }
 };
 
-
 const login_Get = async (req, res) => {
   const data = await UserModel.find();
   res.send({ data });
 };
-
 
 module.exports = { Form, login, login_Get };
